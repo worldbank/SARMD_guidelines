22ace9df42a3678a14b32859e9fd1c5fa09deea9
Andres Castaneda <acastanedaa@worldbank.org>
2019-05-03 14:08:11 -0400

    include render the whole book and serve the book

---
+++ building.R
@@ -0,0 +1,7 @@
+bookdown::render_book("index.Rmd", "bookdown::gitbook", 
+                      preview = TRUE)
+
+bookdown::render_book("index.Rmd", "bookdown::gitbook")
+
+dir <- getwd()
+bookdown::serve_book(dir = dir, output_dir = "_book")
