4f818d18fe022466ae2afd75847fd03e9f062063
Andres Castaneda <acastanedaa@worldbank.org>
2019-06-11 08:20:58 -0400

    Add Stata engine

--- _common.R
+++ _common.R
@@ -1,6 +1,19 @@
 set.seed(1234)
 options(digits = 3)
 
+# Stata Engine
+
+if (Sys.info()[7] == "wb384996") {   # Andres
+  stata_eng <- "c:/Program Files (x86)/Stata15/StataMP-64.exe"
+} else if (Sys.info()[7] == "wb459082") {  # Javier
+  stata_eng <- ""
+} else if (Sys.info()[7] == "wb502818") { # Jayne
+  stata_eng <- ""
+} else {
+  stata_eng <- ""
+}
+
+
 knitr::opts_chunk$set(
   # comment = "#>",
   # comment=NA,
@@ -11,7 +24,10 @@ knitr::opts_chunk$set(
   fig.align = 'center',
   fig.width = 6,
   fig.asp = 0.618,  # 1 / phi
-  fig.show = "hold"
+  fig.show = "hold",
+  engine.path = list(
+    stata = stata_eng
+  )
 )
 
 options(dplyr.print_min = 6, dplyr.print_max = 6)
