1f741d4f9f9a91e07255a87c739e258553ce7137
Andres Castaneda <acastanedaa@worldbank.org>
2019-06-25 15:06:29 -0400

    update index and var derivation chapter

--- building.R
+++ building.R
@@ -1,6 +1,6 @@
 
 # preview just one chapter
-file <- c("var_derivation.Rmd")
+file <- c("NoteD.Rmd")
 bookdown::render_book(file, "bookdown::gitbook",
                       preview = TRUE)
 beepr::beep(10)
